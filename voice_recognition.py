c8ebae9dbc3949ba1068fc292e31e9314b84629d
Anmol Rathore <anmolrathore530530@gmail.com>
2025-05-18 19:33:37 +0530

    Create voice_recognition.py

---
+++ voice_recognition.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+def listen_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio)
+        print("You said:", command)
+        return command.lower()
+    except:
+        return ""
